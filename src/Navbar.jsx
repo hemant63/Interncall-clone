931ee551047799c5459fa050680e2f07cabbabd7
Hemant <palhemant663@gmail.com>
2024-03-04 21:21:39 +0530

    update code

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -8,6 +8,7 @@ function Navbar() {
     JSON.parse(localStorage.getItem("data")) || []
   );
   console.log("test");
+  console.log("hello2");
   //   const handleSubmit = (e) => {
   //     e.preventDefault();
 
